bc929ba70b7b175ce17f76da37b4b3ffb9529cb2
Sami Fawcett <samifawcett.nyc@gmail.com>
2025-01-26 23:11:29 -0800

    empty window created

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main(int argc, char const *argv[])
-{
-    std::cout << "Hello world!" << std::endl;
-    return EXIT_SUCCESS;
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Test");
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+    return 0;
 }
